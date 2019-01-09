cd6686e5c9faef21414632f19f3f68d080b8218b
Cody Rickman <cody@creativemindstudios.com>
2019-01-09 18:31:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,7 +41,8 @@ class Song
   def self.new_from_filename(file_name)
     song = self.new
     parsed = file_name.split(/-/)
-    song.name = parsed[1].split('.')
+    parsed2 = parsed[1].split('.')
+    song.name = parsed2[0]
     song.artist_name = parsed[0]
     return song
   end
